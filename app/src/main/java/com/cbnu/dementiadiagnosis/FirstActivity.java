be057f617e8ed30ee1bcad66164f0dfccf69a948
tnrl99 <xcv6694@naver.com>
2022-11-17 23:36:16 +0900

    시작 시, 모션 추가

--- app/src/main/java/com/cbnu/dementiadiagnosis/FirstActivity.java
+++ app/src/main/java/com/cbnu/dementiadiagnosis/FirstActivity.java
@@ -5,15 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import user.SharedPreference;
 
 public class FirstActivity extends AppCompatActivity {
 
+    Animation ani;
+    ImageView img;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+        img = findViewById(R.id.img);
+        ani = AnimationUtils.loadAnimation(this, R.anim.pop);
+        img.startAnimation(ani);
 
         Handler hand = new Handler();
 
